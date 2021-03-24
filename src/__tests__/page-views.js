9133c5dff4712e43d1aae3f654aa461211c48500
Niklas Dahlqvist <dalkmania@gmail.com>
2021-03-24 15:34:15 -0700

    Adding More Tests and Clean Up

--- src/__tests__/page-views.js
+++ src/__tests__/page-views.js
@@ -6,73 +6,24 @@ jest.useFakeTimers("modern").setSystemTime(new Date("2019-10-30").getTime());
 describe("Pageviews", () => {
     beforeEach(() => {
         delete window.location;
-    });
-
-    describe("UTMPurser.create()", () => {
-        it(`It creates a Purser Visitor Object with UTM params based on a real URL`, () => {
-            window.location = {
-                toString: () =>
-                    "http://example.com?utm_source=example&utm_content=content&utm_medium=medium&utm_campaign=campaign&utm_name=name&utm_tem=term",
-                origin: "http://example.com",
-                pathname: "/"
-            };
-
-            const expected = {
-                referrer: "direct",
-                browser_timezone: 0,
-                browser_language: "en-US",
-                landing_page: "http://example.com/",
-                last_visit: 1572393600,
-                pageviews: 1,
-                first_website_visit: "2019-10-30T00:00:00.000Z",
-                screen_height: 0,
-                screen_width: 0,
-                utm_source: "example",
-                utm_medium: "medium",
-                utm_campaign: "campaign",
-                utm_content: "content",
-                utm_name: "name"
-            };
-
-            expect(UTMPurser.create()).toStrictEqual(expected);
-        });
+        window.location = {
+            toString: () =>
+                "http://example.com?utm_source=example&utm_content=content&utm_medium=medium&utm_campaign=campaign&utm_name=name&utm_tem=term",
+            origin: "http://example.com",
+            pathname: "/"
+        };
+        UTMPurser.create();
     });
 
     describe("Pageviews.add()", () => {
         it(`It increments the pageview count`, () => {
             window.location = {
-                toString: () =>
-                    "http://example.com?utm_source=example&utm_content=content&utm_medium=medium&utm_campaign=campaign&utm_name=name&utm_tem=term",
-                origin: "http://example.com",
-                pathname: "/"
-            };
-
-            let expected = {
-                referrer: "direct",
-                browser_timezone: 0,
-                browser_language: "en-US",
-                landing_page: "http://example.com/",
-                last_visit: 1572393600,
-                pageviews: 1,
-                first_website_visit: "2019-10-30T00:00:00.000Z",
-                screen_height: 0,
-                screen_width: 0,
-                utm_source: "example",
-                utm_medium: "medium",
-                utm_campaign: "campaign",
-                utm_content: "content",
-                utm_name: "name"
-            };
-
-            expect(UTMPurser.create()).toStrictEqual(expected);
-
-            window.location = {
                 toString: () => "http://example.com/other-page",
-                origin: "http://example.com",
+                origin: "http://example.com/other-page",
                 pathname: "/"
             };
 
-            expected = {
+            const expected = {
                 referrer: "direct",
                 browser_timezone: 0,
                 browser_language: "en-US",
--- src/__tests__/visits.js
+++ src/__tests__/visits.js
@@ -6,36 +6,13 @@ jest.useFakeTimers("modern").setSystemTime(new Date("2019-10-30").getTime());
 describe("Visits", () => {
     beforeEach(() => {
         delete window.location;
-    });
-
-    describe("UTMPurser.create()", () => {
-        it(`It creates a Purser Visitor Object with UTM params based on a real URL`, () => {
-            window.location = {
-                toString: () =>
-                    "http://example.com?utm_source=example&utm_content=content&utm_medium=medium&utm_campaign=campaign&utm_name=name&utm_tem=term",
-                origin: "http://example.com",
-                pathname: "/"
-            };
-
-            const expected = {
-                referrer: "direct",
-                browser_timezone: 0,
-                browser_language: "en-US",
-                landing_page: "http://example.com/",
-                last_visit: 1572393600,
-                pageviews: 1,
-                first_website_visit: "2019-10-30T00:00:00.000Z",
-                screen_height: 0,
-                screen_width: 0,
-                utm_source: "example",
-                utm_medium: "medium",
-                utm_campaign: "campaign",
-                utm_content: "content",
-                utm_name: "name"
-            };
-
-            expect(UTMPurser.create()).toStrictEqual(expected);
-        });
+        window.location = {
+            toString: () =>
+                "http://example.com?utm_source=example&utm_content=content&utm_medium=medium&utm_campaign=campaign&utm_name=name&utm_tem=term",
+            origin: "http://example.com",
+            pathname: "/"
+        };
+        UTMPurser.create();
     });
 
     describe("Visits.recently()", () => {
@@ -50,6 +27,58 @@ describe("Visits", () => {
             const mockDate = new Date(1572397080000);
             const spy = jest.spyOn(global, "Date").mockImplementation(() => mockDate);
             expect(Visits.recently()).toBe(false);
+            spy.mockRestore();
+        });
+    });
+
+    describe("Visits.create()", () => {
+        it(`It creates a new visit in the visits array, when more than 30 minutes has passed`, () => {
+            window.location = {
+                toString: () =>
+                    "http://example.com/other-page/?utm_source=example&utm_content=content&utm_medium=medium&utm_campaign=campaign&utm_name=name&utm_tem=term",
+                origin: "http://example.com/other-page",
+                pathname: "/"
+            };
+
+            const expected = {
+                referrer: "direct",
+                browser_timezone: 0,
+                browser_language: "en-US",
+                landing_page: "http://example.com/",
+                last_visit: 1572397080,
+                pageviews: 1,
+                first_website_visit: "2019-10-30T00:00:00.000Z",
+                screen_height: 0,
+                screen_width: 0,
+                utm_source: "example",
+                utm_medium: "medium",
+                utm_campaign: "campaign",
+                utm_content: "content",
+                utm_name: "name",
+                visits: [
+                    {
+                        referrer: "direct",
+                        browser_timezone: 0,
+                        browser_language: "en-US",
+                        landing_page: "http://example.com/other-page/",
+                        last_visit: 1572397080,
+                        pageviews: 1,
+                        date: "2019-10-30T00:58:00.000Z",
+                        first_website_visit: "2019-10-30T00:58:00.000Z",
+                        screen_height: 0,
+                        screen_width: 0,
+                        utm_source: "example",
+                        utm_medium: "medium",
+                        utm_campaign: "campaign",
+                        utm_content: "content",
+                        utm_name: "name"
+                    }
+                ]
+            };
+
+            const mockDate = new Date(1572397080000);
+            const spy = jest.spyOn(global, "Date").mockImplementation(() => mockDate);
+            expect(Visits.create()).toMatchObject(expected);
             spy.mockRestore();
         });
     });
